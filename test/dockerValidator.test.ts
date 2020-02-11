a7b25af4e02ff9de3b6f052575bc5cb1334c5ad5
Remy Suen <remy.suen@gmail.com>
2020-02-11 17:59:55 -0500

    Fix #69 Handle one length argument on the last line
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- test/dockerValidator.test.ts
+++ test/dockerValidator.test.ts
@@ -3728,6 +3728,12 @@ describe("Docker Validator Tests", function() {
 
             diagnostics = validateDockerfile("FROM busybox\r\nRUN ls && \\\r\n");
             assert.equal(diagnostics.length, 0);
+
+            diagnostics = validateDockerfile("FROM busybox\r\nRUN ls && \\\na");
+            assert.equal(diagnostics.length, 0);
+
+            diagnostics = validateDockerfile("FROM busybox\r\nRUN ls && \\\r\na");
+            assert.equal(diagnostics.length, 0);
         });
 
         it("whitespace newline escape", function() {
